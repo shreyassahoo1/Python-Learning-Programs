f471aa176fc9b8853bd3b25f883eead3712f7d62
Shreyas Sahoo <shreyas0502@gmail.com>
2026-05-30 12:01:01 +0530

    Add nested loops to print triangular pattern

---
+++ forloop1.py
@@ -0,0 +1,15 @@
+# n=10
+# for i in range(2 , n+1, 3):
+#     print(i)
+
+# n=5
+# for i in range(1 , n+1):
+#     for j in range(1 , n+1):
+#         print(j, end=" ") 
+#     print(end= "\n")
+
+n=30
+for i in range(1 , n+1):
+    for j in range(1 , i+1):
+        print(j, end=" ") 
+    print(end= "\n")
