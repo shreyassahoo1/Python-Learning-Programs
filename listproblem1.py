f0933eeb13487c991ba3571ce65c03a72c22ee22
Shreyas Sahoo <shreyas0502@gmail.com>
2026-05-30 12:07:00 +0530

    Add files via upload

---
+++ listproblem1.py
@@ -0,0 +1,28 @@
+lt = ["tesla", "kia", "ferrari"]
+print(lt)
+
+lt[1] = "honda"
+print(lt)
+
+lt.extend(["sedan","suv","sports car"])
+print(lt)
+
+print(lt[2])
+
+lt[2]="Cars"
+print(lt)
+
+lt.append(4518)
+print(lt)
+
+lt.insert(2,"Bugatti")
+print(lt)
+
+lt.remove("honda")
+print(lt)
+
+lt.reverse()
+print(lt)
+
+lt.clear()
+print(lt)
