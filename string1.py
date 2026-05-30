805d05a53e51ec963f5976d8eac6467a7dfc5133
Shreyas Sahoo <shreyas0502@gmail.com>
2026-05-30 12:09:25 +0530

    Add files via upload

---
+++ string1.py
@@ -0,0 +1,14 @@
+txt="happy"
+x=txt.center(100,"*")
+print(x)
+x=txt.ljust(20,"0")
+print(x)
+x=txt.rjust(100,"b")
+print(x)
+x=txt.zfill(20)
+print(x)
+txt1="I like cricket and football"
+y=txt1.rfind("l")
+print(y)
+z=txt1.rindex("l")
+print(z)
