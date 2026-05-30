8a087e45ab3843932b49fe800019e454ed12dbe2
Shreyas Sahoo <shreyas0502@gmail.com>
2026-05-30 12:02:34 +0530

    Add files via upload

---
+++ fullpyramid.py
@@ -0,0 +1,13 @@
+n = 7
+i=0
+while i < n:
+    j=0
+    if j < n-i:
+        print(" ", end=" ")
+    j+=1
+    k=0
+    while k < i+1:
+        print(" * ", end=" ")
+        k+=1
+    print()
+    i+=1
