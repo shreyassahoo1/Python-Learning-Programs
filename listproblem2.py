1325df1f08a1ebfe214c17ec41bce6efe9ab1dfc
Shreyas Sahoo <shreyas0502@gmail.com>
2026-05-30 12:07:11 +0530

    Add files via upload

---
+++ listproblem2.py
@@ -0,0 +1,25 @@
+lt = [10,20,30,40,50]
+print("This is the first List: ", lt)
+
+lt.remove(20)
+print(lt)
+
+lt.pop(2)
+print(lt)
+
+lt.pop
+
+lt1 = [5,3,9,10,43,20,17,2,6]
+print("This is the second list: ", lt1)
+
+lt1.sort()
+print(lt1)
+
+lt1.sort(reverse = True)
+print(lt1)
+
+lt1.reverse
+print(lt1)
+
+lt1.extend([3,4,12,11])
+print(lt1)
