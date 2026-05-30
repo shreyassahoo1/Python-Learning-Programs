cb4015a314a06af4a939e7696812f510788ebe1e
Shreyas Sahoo <shreyas0502@gmail.com>
2026-05-30 12:05:28 +0530

    Add files via upload

---
+++ invertedhalf.py
@@ -0,0 +1,16 @@
+n = 6
+
+i=0
+while i < n:
+    j=0
+    while j < i:
+        print(" ", end=" ")
+        j+=1
+        
+    k=0
+    while k < n-i:
+        print("*", end=" ")
+        k+=1
+    
+    print()
+    i+=1
